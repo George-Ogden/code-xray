8518c52a0db4165008de012958d1a2c9e0eda3cc
George Ogden <george-ogden@outlook.com>
2024-05-21 09:22:17 +0100

    collect and run tests with debugger

--- bundled/tool/xray/test_filter.py
+++ bundled/tool/xray/test_filter.py
@@ -1,12 +1,15 @@
 from __future__ import annotations
 
 import os.path
+from typing import Optional
 
 import pytest
 
+from .debugger import Debugger
+
 
 class TestFilter:
-    def __init__(self, filepath: str, function_name: str):
+    def __init__(self, filepath: str, function_name: str, debugger: Optional[Debugger] = None):
         root, extension = os.path.splitext(filepath)
         dirname, basename = os.path.split(root)
         self.acceptable_filepaths = {
@@ -19,6 +22,7 @@ class TestFilter:
             f"{function_name}_test",
         }
         self.tests: list[pytest.Item] = []
+        self.debugger = debugger
 
     def _filter(self, session: pytest.Session, test: pytest.Function) -> bool:
         function_name = test.originalname
@@ -32,16 +36,36 @@ class TestFilter:
     def pytest_collection_modifyitems(
         self, session: pytest.Session, config: pytest.Config, items: list[pytest.Item]
     ):
-        session.items = [test for test in items if self._filter(session, test)]
+        items[:] = [test for test in items if self._filter(session, test)]
+        session.items = items
         session.testscollected = len(session.items)
-        self.tests = session.items
-        return session
+        self.tests = items
+        if self.debugger is None:
+            return True
 
     def collect_tests(self):
         pytest.main(["-qq", "--co"], plugins=[self])
 
     @classmethod
     def get_tests(cls, filepath: str, function_name: str) -> list[pytest.Item]:
+        """Return a list of collected items."""
         plugin = cls(filepath=filepath, function_name=function_name)
         plugin.collect_tests()
         return plugin.tests
+
+    def pytest_runtest_setup(self, item: pytest.Item):
+        if self.debugger is not None:
+            self.debugger.set_trace()
+
+    def pytest_runtest_teardown(self, item: pytest.Item):
+        if self.debugger is not None:
+            self.debugger.set_quit()
+
+    def collect_and_run_tests(self):
+        pytest.main(["-qq"], plugins=[self])
+
+    @classmethod
+    def run_tests(cls, debugger: Debugger, filepath: str, function_name: str) -> list[pytest.Item]:
+        plugin = cls(filepath=filepath, function_name=function_name, debugger=debugger)
+        plugin.collect_and_run_tests()
+        return debugger.difference_logs
