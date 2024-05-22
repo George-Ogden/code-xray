06fc21d1c61a8350eb55acc8d34f864da449d3f7
George Ogden <george-ogden@outlook.com>
2024-05-22 22:03:20 +0100

    implement core of module

--- bundled/tool/xray/__main__.py
+++ bundled/tool/xray/__main__.py
@@ -1,2 +1,59 @@
+import argparse
+import sys
+from dataclasses import dataclass
+from typing import Any, Dict
+
+from .debugger import Debugger
+from .parsing_utils import Config, ParserBuilder
+from .test_filter import TestFilter
+
+
+@dataclass
+class TracingConfig(Config):
+    filepath: str
+    function: str
+    lineno: int
+
+
+def parse_args() -> argparse.Namespace:
+    parser_builder = ParserBuilder()
+    parser_builder.add_dataclass(TracingConfig)
+    parser = parser_builder.build()
+    args = parser.parse_args()
+    return args
+
+
+def parse_config() -> TracingConfig:
+    args = parse_args()
+    return TracingConfig.from_args(args)
+
+
+def log(input: Any, /, **kwargs: Any):
+    """Log debugging information (use like print)."""
+    print(input, **kwargs, file=sys.stderr)
+
+
+def send(annotations: Dict[int, str]):
+    """Send back the annotations."""
+    for line, annotation in annotations:
+        print(f"{line}:{annotation}")
+
+
+def main(config: TracingConfig):
+    filepath = config.filepath
+    function_name = config.function
+    lineno = config.lineno
+    potential_tests = TestFilter.get_tests(filepath=filepath, function_name=function_name)
+    log(
+        f"Found {len(potential_tests)} potential tests: [{','.join(test.name for test in potential_tests)}]"
+    )
+    debugger = Debugger(filepath, lineno)
+    annotations = TestFilter.run_tests(debugger, filepath, function_name)
+    log(debugger.frame)
+    log(f"Annotating {len(annotations)} lines in {filepath}.")
+    send(annotations)
+
+
 if __name__ == "__main__":
-    print("xray-module")
+    config = parse_config()
+    main(config)
