b7346b959da6d1dd719e279d3b01faf7e91d5e8d
George Ogden <george-ogden@outlook.com>
2024-06-09 15:36:44 +0100

    stop using xray as a module
    no longer used and not tested

--- bundled/tool/xray/utils/__init__.py
+++ bundled/tool/xray/utils/__init__.py
@@ -1,6 +1,5 @@
 from .config import Config
 from .line_number import LineNumber
-from .parser import ParserBuilder
 from .serializable import Serializable
 from .silence_output import silence_output
 from .tying import renamable
