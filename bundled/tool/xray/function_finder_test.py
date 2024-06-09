41bb7d36e8a9b9e77895d0eed51f3aace219c271
George Ogden <george-ogden@outlook.com>
2024-06-09 15:04:13 +0100

    test function finder with new document

---
+++ bundled/tool/xray/function_finder_test.py
@@ -0,0 +1,22 @@
+import os.path
+
+import pytest
+
+from . import FunctionFinder
+from .utils import LineNumber
+
+
+@pytest.mark.parametrize(
+    "filename,lineno,name",
+    [
+        ("tests/quicksort.py", 1, "unused_fn1"),
+        ("tests/quicksort.py", 4, "sort"),
+        ("tests/quicksort.py", 28, "unused_fn2"),
+    ],
+)
+def test_function_finder(filename: str, lineno: int, name: str):
+    with open(os.path.join(os.path.dirname(__file__), filename)) as f:
+        source = f.read()
+
+    line_number = LineNumber[1](lineno)
+    assert FunctionFinder.find_function(source, line_number) == name
