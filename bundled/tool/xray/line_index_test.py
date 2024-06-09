5f7e13f5d366108476300580f9d294c4f132d0aa
George Ogden <george-ogden@outlook.com>
2024-06-09 14:55:47 +0100

    test line index builder

---
+++ bundled/tool/xray/line_index_test.py
@@ -0,0 +1,22 @@
+import os.path
+
+import pytest
+
+from . import LineIndexBuilder
+from .utils import LineNumber
+
+
+@pytest.mark.parametrize(
+    "filename,lineno,partial_index", [("tests/quicksort.py", 4, {8: 9, 10: 12, 22: 24})]
+)
+def test_line_index_builder(filename: str, lineno: int, partial_index: dict[int, int]):
+    """`partial_index` contains all line numbers that should be different (one-based indexing)"""
+    line_number = LineNumber[1](lineno)
+
+    with open(os.path.join(os.path.dirname(__file__), filename)) as f:
+        source = f.read()
+
+    index = LineIndexBuilder.build_index(source, line_number)
+
+    for k, v in index.items():
+        assert k == v or partial_index[k.one] == v.one
