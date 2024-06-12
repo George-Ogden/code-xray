5fe4305ef34cb32d0cd677797d2d082f9f7402bb
George Ogden <george-ogden@outlook.com>
2024-06-12 11:56:10 +0100

    rewrite line index to accept a node
    previously line number and source

--- bundled/tool/xray/line_index.py
+++ bundled/tool/xray/line_index.py
@@ -18,29 +18,29 @@ class LineIndex(dict[LineNumber, LineNumber]):
 class LineIndexBuilder(ast.NodeVisitor):
     """Compute an index to store a map from to last lines of definitions in a file."""
 
-    class FunctionAnalyzedException(Exception): ...
-
-    def __init__(self, line_number: LineNumber):
-        self.line_number = line_number
-        # Leave end line number until it is computed.
-        self.end_line_number: Optional[LineNumber] = None
+    def __init__(self, node: ast.FunctionDef):
         # Map from start lines to end lines.
         self.index = LineIndex()
+        self.root = node
+
+    def visit(self, node: Optional[ast.AST] = None):
+        if node is None:
+            # The root is the only function definition to not visit.
+            for child in ast.iter_child_nodes(self.root):
+                super().visit(child)
+        else:
+            super().visit(node)
 
     def generic_visit(self, node: ast.AST):
         try:
             start_line_number = LineNumber[1](node.lineno)
-            # Ignore lines after the end of the function.
-            if start_line_number > self.end_lineno:
-                return
-            # Ignore lines before the start of the function.
             end_line_number = LineNumber[1](node.end_lineno)
-            if end_line_number < self.line_number:
-                return
+        except AttributeError:
+            ...
+        else:
             match node:
                 case (
                     ast.AsyncFor()
-                    | ast.AsyncFunctionDef()
                     | ast.AsyncWith()
                     | ast.For()
                     | ast.If()
@@ -56,25 +56,11 @@ class LineIndexBuilder(ast.NodeVisitor):
                 case _:
                     # We must be in range so update the index.
                     self.index.update(start_line_number, end_line_number)
-        except AttributeError:
-            ...
         super().generic_visit(node)
 
-    def visit_FunctionDef(self, node: ast.FunctionDef):
-        if LineNumber[1](node.lineno) == self.line_number:
-            # If this is the function, update the end line number, visit then finish (raise an exception).
-            self.end_lineno = LineNumber[1](node.end_lineno)
-            super().generic_visit(node)
-            raise LineIndexBuilder.FunctionAnalyzedException(node)
-
     @classmethod
-    def build_index(cls, source: str, line_number: LineNumber) -> LineIndex:
+    def build_index(cls, node: ast.FunctionDef) -> LineIndex:
         """Build the index from line numbers to source numbers."""
-        tree = ast.parse(source)
-
-        finder = LineIndexBuilder(line_number=line_number)
-        try:
-            finder.visit(tree)
-        except LineIndexBuilder.FunctionAnalyzedException:
-            ...
+        finder = LineIndexBuilder(node)
+        finder.visit()
         return finder.index
