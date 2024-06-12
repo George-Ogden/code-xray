e4d32b413a516bf12aca5596dedc61779bd918c4
George Ogden <george-ogden@outlook.com>
2024-06-12 13:35:47 +0100

    integrate control index and function nodes into debugger

--- bundled/tool/xray/debugger.py
+++ bundled/tool/xray/debugger.py
@@ -1,14 +1,15 @@
+import ast
 import bdb
 import copy
 import enum
-import re
 from typing import Union
 
 from .annotation import Position
 from .config import File
+from .control_index import ControlIndexBuilder
 from .difference import *
 from .differences import Differences
-from .indent_index import IndentIndex
+from .indent_index import IndentIndex, IndentIndexBuilder
 from .line_index import LineIndex, LineIndexBuilder
 from .utils import LineNumber
 
@@ -19,40 +20,44 @@ class FrameState(enum.Enum):
 
 
 class Debugger(bdb.Bdb):
-    def __init__(self, file: File, method_lineno: LineNumber, skip=None) -> None:
+    def __init__(self, file: File, node: ast.FunctionDef, skip=None) -> None:
         super().__init__(skip)
         # Canonicalize filename.
         self._filename = self.canonic(file.filepath)
         self._source = file.source
-        self._line_number = method_lineno
+        self._line_number = LineNumber[1](node.lineno)
 
         # Initialise differences and previous lines.
-        self.previous_line: int
+        self.previous_position: Position
         self.differences = Differences()
 
         self.frame: Union[FrameState, "frame"] = FrameState.UNINITIALIZED
 
         # Build indices.
-        self._line_index = self.precompute_line_index(file)
+        self._line_index = self.precompute_line_index(node)
         self._indent_index = self.precompute_indent_index(file)
+        self._control_index = self.precompute_control_index(node)
 
         # Initialise locals.
         self._locals = {}
         super().run("", self._locals)
 
-    def precompute_line_index(self, file: File) -> LineIndex:
-        return LineIndexBuilder.build_index(file.source, self._line_number)
+    def precompute_line_index(self, node: ast.FunctionDef) -> LineIndex:
+        return LineIndexBuilder.build_index(node)
+
+    def precompute_control_index(self, node: ast.FunctionDef) -> LineIndex:
+        return ControlIndexBuilder.build_index(node)
 
     def precompute_indent_index(self, file: File) -> IndentIndex:
-        # Compute the number of spaces at the beginning of each line.
-        spaces = [re.match(r"^ *", line).group(0) for line in file.source.splitlines()]
-        indent_index = {LineNumber[0](i): len(space) for i, space in enumerate(spaces)}
-        return indent_index
+        return IndentIndexBuilder.build_index(file.source)
 
-    def frame_line_number(self, frame) -> int:
+    def frame_position(self, frame) -> Position:
         """Get the line number of the code."""
         line_number = LineNumber[1](frame.f_lineno)
-        return self._line_index[line_number]
+        # Lookup indent and line number in index.
+        indent = self._indent_index[line_number]
+        line_number = self._line_index[line_number]
+        return Position(line_number, indent)
 
     def trace_dispatch(self, frame, event, arg):
         return super().trace_dispatch(frame, event, arg)
@@ -60,11 +65,11 @@ class Debugger(bdb.Bdb):
     def user_line(self, frame) -> None:
         if frame is self.frame:
             # Line number is the entering line, not the exiting one.
-            self.annotate_difference(self.previous_line, frame.f_locals, self._locals)
+            self.annotate_difference(self.previous_position, frame.f_locals, self._locals)
             self._locals = copy.deepcopy(frame.f_locals)
 
             # Update to the next line number.
-            self.previous_line = self.frame_line_number(frame)
+            self.previous_position = self.frame_position(frame)
         return super().user_line(frame)
 
     def user_call(self, frame, argument_list) -> None:
@@ -78,27 +83,27 @@ class Debugger(bdb.Bdb):
             ):
                 # Store the frame if it matches.
                 self.frame = frame
-                self.previous_line = self.frame_line_number(frame)
+                self.previous_position = self.frame_position(frame)
 
-                self.annotate_difference(self.previous_line, frame.f_locals, self._locals)
+                self.annotate_difference(self.previous_position, frame.f_locals, self._locals)
                 self._locals = copy.deepcopy(frame.f_locals)
 
         return super().user_call(frame, argument_list)
 
     def user_return(self, frame, return_value) -> None:
         if frame is self.frame:
-            line_number = self.frame_line_number(frame)
+            position = self.frame_position(frame)
             difference = Return(return_value)
-            self.save_difference(difference, line_number)
+            self.save_difference(difference, position)
             self.frame = FrameState.RETURNED
         return super().user_return(frame.f_code.co_filename, return_value)
 
     def user_exception(self, frame, exc_info) -> None:
         if frame is self.frame:
-            line_number = self.frame_line_number(frame)
+            position = self.frame_position(frame)
             exception, value, traceback = exc_info
             difference = Exception_(value)
-            self.save_difference(difference, line_number)
+            self.save_difference(difference, position)
             self.frame = FrameState.RETURNED
         return super().user_exception(frame, exc_info)
 
@@ -114,6 +119,9 @@ class Debugger(bdb.Bdb):
         )
         self.save_difference(difference, line_number)
 
-    def save_difference(self, difference: Difference, line_number: LineNumber):
+    def save_difference(self, difference: Difference, position: Position):
         """Save the difference."""
-        self.differences.add(Position(line_number, self._indent_index[line_number]), difference)
+        self.differences.add(position, difference)
+
+    def get_annotations(self):
+        return self.differences.to_annotations(self._control_index)
