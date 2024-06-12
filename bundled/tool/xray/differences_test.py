1f4e5646aba8344dd87f6f186218a875bc7ca946
George Ogden <george-ogden@outlook.com>
2024-06-12 12:53:59 +0100

    update differences test

--- bundled/tool/xray/differences_test.py
+++ bundled/tool/xray/differences_test.py
@@ -1,45 +1,45 @@
+import itertools
+
 import pytest
 
-from . import Differences
+from . import ControlIndex, Differences
+from .control_index import ControlNode
 from .difference import NoDifference
 from .utils import LineNumber, Position
 
 
 @pytest.mark.parametrize(
-    "positions,expected_timestamps",
+    "positions,expected_timestamps,control_index",
     [
-        ([(1, 4), (2, 4), (3, 4)] * 2, [((1, 0),), ((1, 1),)]),
+        ([(1, 4), (2, 4), (3, 4)] * 2, [(1,), (2,)], {1: 1, 2: 1, 3: 1}),
         (
             [(1, 4), (2, 8), (3, 8), (2, 8), (3, 8)] * 2,
-            [
-                ((1, 0),),
-                (
-                    (1, 0),
-                    (2, 0),
-                ),
-                (
-                    (1, 0),
-                    (2, 1),
-                ),
-                ((1, 1),),
-                (
-                    (1, 1),
-                    (2, 0),
-                ),
-                (
-                    (1, 1),
-                    (2, 1),
-                ),
-            ],
+            [(1,), (1, 1), (1, 2), (2,), (2, 1), (2, 2)],
+            {1: 1, 2: 2, 3: 2},
         ),
         (
-            [(1, 4), (2, 8), (3, 8), (1, 4), (2, 8), (4, 4), (5, 8), (1, 4), (2, 8), (3, 8)],
-            [((1, 0),), ((1, 1),), ((1, 2),)],
+            [
+                (1, 4),
+                (2, 8),
+                (3, 8),
+                (1, 4),
+                (2, 8),
+                (5, 8),
+                (1, 4),
+                (2, 8),
+                (3, 8),
+            ],
+            [(1,), (2,), (3,)],
+            {1: 1, 2: 1, 3: 1, 4: 1, 5: 1},
         ),
-        ([(1, 0), (2, 4), (3, 4), (2, 4), (3, 4)], [((1, 0),), ((1, 1),)]),
+        (
+            [(1, 0), (2, 4), (3, 4), (2, 4), (3, 4)],
+            [(), (1,), (2,)],
+            {1: 0, 2: 2, 3: 2},
+        ),
     ],
 )
-def test_difference_groups(positions, expected_timestamps):
+def test_difference_groups(positions, expected_timestamps, control_index: ControlIndex):
     """
     Test 1:
     for i in range(2):
@@ -68,10 +68,20 @@ def test_difference_groups(positions, expected_timestamps):
         position = Position(LineNumber[1](lineno), character)
         differences.add(position, NoDifference())
 
-    groups = differences.group()
+    node = None
+    index = {}
+    for line, target in itertools.chain([(0, None)], control_index.items()):
+        line_number = LineNumber[1](line)
+        if target is None:
+            node = ControlNode(None, line_number=line_number)
+        elif line == target:
+            node = ControlNode(node, line_number=line_number)
+        index[line_number] = node
+
+    groups = differences.group(index)
 
     # Check the timestamps match.
-    assert set(groups.keys()) == set(expected_timestamps).union([()])
+    assert set(groups.keys()) == set(expected_timestamps)
 
-    # Check all the positions are there (plus an initial position).
-    assert sum([len(group) for group in groups.values()]) == len(positions) + 1
+    # Check all the positions are there.
+    assert sum([len(group) for group in groups.values()]) == len(positions)
