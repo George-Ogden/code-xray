f7e323f7820da936ac2c446ecff1ebce1375f09d
George Ogden <george-ogden@outlook.com>
2024-06-13 11:59:55 +0100

    define test with edge cases

---
+++ bundled/tool/xray/tests/edge_cases.py
@@ -0,0 +1,32 @@
+def main(
+        arg1=1,
+        arg2=2
+):
+    """Sort the array by using quicksort."""
+    list_comp = [
+        2 * i for i in range(3)
+    ]
+    set_comp = {
+        i + 1
+        for i
+        in range(3)
+    }
+
+    generator = (x * 2 for x in range(3))
+    for x in generator:
+        y = x + 1
+
+    for i in [1,2]:
+        for j in [3,4]:
+            z = (i, j)
+
+    try:
+        a = 2
+    except KeyboardInterrupt:
+        b = 3
+    except SyntaxError:
+        c = 4
+    else:
+        d = 5
+    finally:
+        e = 6
